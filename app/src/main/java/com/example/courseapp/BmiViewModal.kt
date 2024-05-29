1e68780e1f7472533a10df12f25d44d70c5abfaf
teotia-codes <priyanshu.teotia50@gmail.com>
2024-05-29 17:26:26 +0530

    checkpoint

--- app/src/main/java/com/example/courseapp/BmiViewModal.kt
+++ app/src/main/java/com/example/courseapp/BmiViewModal.kt
@@ -6,37 +6,41 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 
-class BmiViewModal: ViewModel(){
+class BmiViewModal : ViewModel() {
     var state by mutableStateOf(BMIState())
-    fun onAction(userAction: UserAction){
-        when(userAction){
-            UserAction.WeightValueClicked->{
+    fun onAction(userAction: UserAction) {
+        when (userAction) {
+            UserAction.WeightValueClicked -> {
                 state = state.copy(
                     sheetTitle = "Weight",
                     sheetItemList = listOf("Kilogram", "Pounds")
                 )
             }
-            UserAction.HeighValueClicked->{
+
+            UserAction.HeighValueClicked -> {
                 state = state.copy(
                     sheetTitle = "Height",
                     sheetItemList = listOf("Centimetres", "metres", "Feet", "Inches")
                 )
             }
+
             is UserAction.OnSheetItemClicked -> {
                 changeWeightOrHeightUnit(userAction.sheetItem)
             }
 
             is UserAction.OnNumberClicked -> {
-                enterNumber(userAction.number )
+                enterNumber(userAction.number)
 
             }
-            UserAction.HeightClicked ->{
+
+            UserAction.HeightClicked -> {
                 state = state.copy(
                     isHeightValueActive = HeightValueStage.ACTIVE,
                     isWeightValueActive = WeightValueStage.INACTIVE
                 )
             }
-            UserAction.WeightClicked ->{
+
+            UserAction.WeightClicked -> {
                 state = state.copy(
                     isWeightValueActive = WeightValueStage.INACTIVE,
                     isHeightValueActive = HeightValueStage.ACTIVE
@@ -44,52 +48,56 @@ class BmiViewModal: ViewModel(){
             }
         }
     }
-    private fun enterNumber(number: String){
-when {
-    state.weightValueStage == WeightValueStage.ACTIVE -> {
-        state = state.copy(
-            weightValue =  if (number == ".") "0." else number,
-            weightValueStage = WeightValueStage.RUNNING
-        )
 
-    }
-    state.weightValueStage == WeightValueStage.RUNNING -> {
-        state = state.copy(
-            weightValue = state.weightValue + number ,
-            weightValueStage =  WeightValueStage.RUNNING
-        )
+    private fun enterNumber(number: String) {
+        when {
+            state.weightValueStage == WeightValueStage.ACTIVE -> {
+                state = state.copy(
+                    weightValue = if (number == ".") "0." else number,
+                    weightValueStage = WeightValueStage.RUNNING
+                )
 
-    }
-    state.heightValueStage == HeightValueStage.ACTIVE -> {
-        state = state.copy(
-            heightValue =  if (number == ".") "0." else number,
-            heightValueStage = HeightValueStage.RUNNING
-        )
+            }
 
-    }
-    state.heightValueStage == HeightValueStage.RUNNING -> {
-        state = state.copy(
-            heightValue = state.heightValue + number ,
-            heightValueStage =  HeightValueStage.RUNNING
-        )
-    }
-}
-    }
-    private fun changeWeightOrHeightUnit(sheetItem: String){
-        if (state.sheetTitle == "Weight"){
-            state = state.copy(weightUnit = sheetItem)
+            state.weightValueStage == WeightValueStage.RUNNING -> {
+                state = state.copy(
+                    weightValue = state.weightValue + number,
+                    weightValueStage = WeightValueStage.RUNNING
+                )
+
+            }
+
+            state.heightValueStage == HeightValueStage.ACTIVE -> {
+                state = state.copy(
+                    heightValue = if (number == ".") "0." else number,
+                    heightValueStage = HeightValueStage.RUNNING
+                )
+
+            }
+
+            state.heightValueStage == HeightValueStage.RUNNING -> {
+                state = state.copy(
+                    heightValue = state.heightValue + number,
+                    heightValueStage = HeightValueStage.RUNNING
+                )
+            }
         }
-        else if (state.sheetTitle == "Height"){
-            state= state.copy(heightUnit = sheetItem)
+    }
+
+    private fun changeWeightOrHeightUnit(sheetItem: String) {
+        if (state.sheetTitle == "Weight") {
+            state = state.copy(weightUnit = sheetItem)
+        } else if (state.sheetTitle == "Height") {
+            state = state.copy(heightUnit = sheetItem)
         }
     }
 }
 
 sealed class UserAction {
-    object WeightValueClicked: UserAction()
-    object  HeighValueClicked: UserAction()
-    object HeightClicked: UserAction()
-    object WeightClicked: UserAction()
-    data class OnSheetItemClicked(val sheetItem: String): UserAction()
-    data class OnNumberClicked(val number: String): UserAction()
+    object WeightValueClicked : UserAction()
+    object HeighValueClicked : UserAction()
+    object HeightClicked : UserAction()
+    object WeightClicked : UserAction()
+    data class OnSheetItemClicked(val sheetItem: String) : UserAction()
+    data class OnNumberClicked(val number: String) : UserAction()
 }
